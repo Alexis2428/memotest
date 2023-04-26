ac2ae8ac8611bffd175424d27abeca8751f4b879
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2023-04-26 18:02:25 -0300

    Agrega funciones para mostrar/ocultar elementos

--- src/main.js
+++ src/main.js
@@ -18,3 +18,22 @@ function reiniciarJuego() {
     alternarEstadoBoton('comenzar', 'oculto');
 }
 
+function mostrarElemento(elemento) {
+    document.querySelector(`#${elemento}`).classList.remove('oculto');
+}
+
+function ocultarElemento(elemento) {
+    document.querySelector(`#${elemento}`).classList.add('oculto');
+}
+
+function alternarEstadoBoton(boton, estado) {   // la función recibe el estado actual del botón como parametro
+    const $boton = document.querySelector(`#${boton}`);
+
+    if (estado === 'oculto') {
+        $boton.classList.add('d-grid');
+        $boton.classList.remove('oculto');
+    } else {
+        $boton.classList.add('oculto');
+        $boton.classList.remove('d-grid');
+    }
+}
