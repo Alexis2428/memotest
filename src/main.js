bcbfb2063135d92d4d154a1ac352abc8f0be31bc
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2023-04-26 18:06:04 -0300

    Agrega funciones para inicializar el juego

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+const $tablero = document.querySelector('#tablero');
+
+let $primerCuadro;
+let turnos;
+
 document.querySelector('#comenzar').onclick = comenzarJuego;
 
 document.querySelector('#reiniciar').onclick = reiniciarJuego;
@@ -16,6 +21,44 @@ function reiniciarJuego() {
     ocultarElemento('texto-fin-juego');
     mostrarElemento('titulo');
     alternarEstadoBoton('comenzar', 'oculto');
+}
+
+function inicializarJuego() {
+    $primerCuadro = null;
+    turnos = 0;
+
+    configurarTablero();
+    asignarColorCuadros(crearColores());
+}
+
+function configurarTablero() {
+    $tablero.querySelectorAll('.columna').forEach(function($elemento) {
+        $elemento.classList.remove('emparejado');
+        $elemento.appendChild(crearCuadro());
+    });
+}
+
+function crearCuadro() {
+    const $cuadro = document.createElement('div');
+    $cuadro.classList.add('h-100', 'cuadro');
+
+    return $cuadro;
+}
+
+function crearColores() {
+    const coloresCuadros = ['rojo', 'verde', 'azul', 'amarillo', 'negro', 'naranja', 'agua', 'rosa'];
+    let coloresRepetidos = coloresCuadros.concat(coloresCuadros);
+    coloresRepetidos.sort(function() {
+        return 0.5 - Math.random();
+    })
+
+    return coloresRepetidos;
+}
+
+function asignarColorCuadros(colores) {
+    $tablero.querySelectorAll('.cuadro').forEach(function($cuadro, i){
+        $cuadro.classList.add(`${colores[i]}`);
+    })
 }
 
 function mostrarElemento(elemento) {
